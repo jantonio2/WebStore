aa1e0bd8fbd5fb79641af43c544b00c16da867f4
jantonio2 <jesusrochavedia@gmail.com>
2021-03-14 00:26:55 -0400

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-store';
+
+  items = ['nicolas', 'julian', 'luis'];
+
+  addItem() {
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
 }
