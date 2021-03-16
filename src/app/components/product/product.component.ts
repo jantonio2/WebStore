283227f6552e721cca0fc544364342e2ec83dc7c
jantonio2 <jesusrochavedia@gmail.com>
2021-03-16 12:51:20 -0400

    Components life cycle

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  Output, 
+  EventEmitter, 
+  OnChanges, 
+  SimpleChanges, 
+  DoCheck, 
+  OnDestroy 
+} from '@angular/core';
 import { Product } from 'src/app/product.model';
 
 @Component({
@@ -6,15 +16,31 @@ import { Product } from 'src/app/product.model';
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class ProductComponent implements OnInit, OnChanges, DoCheck, OnDestroy {
 
   @Input() product!: Product;
   @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+  constructor() { 
+    console.log('1. constructor');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('2. ngOnChanges');
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    console.log('3. ngOnInit');
+  }
+
+  ngDoCheck() {
+    console.log('4. ngDoCheck');
+  }
+
+  ngOnDestroy() {
+    console.log('5. ngOnDestroy');
+  }
 
   addCart() {
     console.log('añadir al carrito');
