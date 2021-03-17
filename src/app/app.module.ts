5495ddfa7978eb32c87bac5101fc52c8556de635
jantonio2 <jesusrochavedia@gmail.com>
2021-03-17 16:23:32 -0400

    Product service done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { SwiperModule } from 'swiper/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './components/product/product.component';
+import { ProductComponent } from './components/products/product/product.component';
 import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
 import { CartComponent } from './components/cart/cart/cart.component';
 import { HighlightDirective } from './directives/highlight/highlight.directive';
@@ -17,6 +17,7 @@ import { HeaderComponent } from './components/home/header/header.component';
 import { FooterComponent } from './components/home/footer/footer.component';
 import { BannerComponent } from './components/home/banner/banner.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './components/products/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
     HeaderComponent,
     FooterComponent,
     BannerComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
