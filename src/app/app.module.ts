ea2c156154fcca9f2ebb47862ba1a83955a246fd
jantonio2 <jesusrochavedia@gmail.com>
2021-03-17 08:33:59 -0400

    Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { ProductComponent } from './components/product/product.component';
 import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
 import { CartComponent } from './components/cart/cart/cart.component';
 import { HighlightDirective } from './directives/highlight/highlight.directive';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { DemoComponent } from './components/demo/demo.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +19,11 @@ import { HighlightDirective } from './directives/highlight/highlight.directive';
     ProductComponent,
     ExponentialPipe,
     CartComponent,
-    HighlightDirective
+    HighlightDirective,
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
