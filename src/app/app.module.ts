aa6018168b2f093bf19bcf21532ef3b556015ece
jantonio2 <jesusrochavedia@gmail.com>
2021-03-17 00:56:51 -0400

    Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,17 @@ import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
-import { CartComponent } from './cart/cart.component';
 import { ExponentialPipe } from './pipes/exponential/exponential.pipe';
+import { CartComponent } from './components/cart/cart/cart.component';
+import { HighlightDirective } from './directives/highlight/highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
+    ExponentialPipe,
     CartComponent,
-    ExponentialPipe
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
