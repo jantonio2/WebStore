af5d8de2c42952b3b34354031cd1e337c76dbc90
jantonio2 <jesusrochavedia@gmail.com>
2021-03-21 02:08:31 -0400

    Angular Material

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -7,6 +7,7 @@ import { ProductDetailComponent } from './components/product-detail/product-deta
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { SharedModule } from './../shared/shared.module';
+import { MaterialModule } from './../material/material.module'
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { SharedModule } from './../shared/shared.module';
   imports: [
     ProductsRoutingModule, 
     SharedModule,
-    CommonModule
+    CommonModule,
+    MaterialModule
   ]
 })
 export class ProductsModule {
