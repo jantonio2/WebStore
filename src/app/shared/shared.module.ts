6b1e210aafa7fea04de247e0929e8e81c362cf05
jantonio2 <jesusrochavedia@gmail.com>
2021-03-26 00:42:54 -0400

    Form contact done

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -24,7 +25,8 @@ import { MaterialModule } from './../material/material.module';
   imports: [
     CommonModule, 
     RouterModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
