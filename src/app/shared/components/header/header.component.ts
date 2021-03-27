1e558129b168e7083eddb4e6f0336d7938bc32dd
jantonio2 <jesusrochavedia@gmail.com>
2021-03-27 15:32:04 -0400

    Using pipes on cart

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { map } from 'rxjs/operators';
+
 import { CartService } from './../../../core/services/cart/cart.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -8,13 +11,13 @@ import { CartService } from './../../../core/services/cart/cart.service';
 })
 export class HeaderComponent implements OnInit {
 
-  total = 0;
+  total$!: Observable<number>;
 
   constructor(private cartService: CartService) {
-    this.cartService.cart$.subscribe(products => {
-      console.log(products);
-      this.total = products.length;
-    })
+    this.total$ = this.cartService.cart$
+    .pipe(
+      map(products => products.length)
+    );
    }
 
   ngOnInit(): void {
