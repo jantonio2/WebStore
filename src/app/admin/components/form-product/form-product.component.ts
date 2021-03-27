b199e0c6215372f7c0ec6e5156b103ef0cc18445
jantonio2 <jesusrochavedia@gmail.com>
2021-03-27 02:06:31 -0400

    Form products styles added

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { ProductsService } from './../../../core/services/products/products.service'
 
 @Component({
   selector: 'app-form-product',
@@ -10,18 +13,31 @@ export class FormProductComponent implements OnInit {
 
   form!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { 
+  constructor(private formBuilder: FormBuilder, private productService: ProductsService,
+    private router: Router) { 
     this.buildForm();
   }
 
   ngOnInit(): void {
   }
 
+  saveProduct(event: Event) {
+    event.preventDefault();
+    if(this.form.valid){
+      const product = this.form.value;
+      this.productService.createProduct(product)
+      .subscribe((newProduct) =>{
+        console.log(newProduct);
+        this.router.navigate(['./admin/products']);
+      });
+    }
+  }
+
   private buildForm() {
     this.form = this.formBuilder.group({
       id: ['',[Validators.required]],
       title: ['',[Validators.required]],
-      price: [0,[Validators.required]],
+      price: ['',[Validators.required]],
       image: '',
       description: ['',[Validators.required]]
     });
